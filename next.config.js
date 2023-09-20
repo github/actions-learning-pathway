990c446cdf2a28d9ce1d40618c1e208f272f011d
Kevin Sundstrom <74620456+kevinsundstrom@users.noreply.github.com>
2023-09-20 14:23:21 -0700

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+
+  images: {
+    unoptimized: true,
+  },
+  
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
